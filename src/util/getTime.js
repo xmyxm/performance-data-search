84cde080fa6bc5919d7eb06d49fe09ae2dfd7865
chenxuanfeng <chenxuanfeng@meituan.com>
2025-01-20 18:47:54 +0800

    feat: up

---
+++ src/util/getTime.js
@@ -0,0 +1,17 @@
+function getTimeText(timeTramp) {
+	return timeTramp < 10 ? String(`0${timeTramp}`) : String(timeTramp)
+}
+
+function getTime(dt) {
+	const date = dt ? new Date(dt) : new Date()
+	const year = getTimeText(date.getFullYear())
+	const month = getTimeText(date.getMonth() + 1)
+	const day = getTimeText(date.getDate())
+	const hours = getTimeText(date.getHours())
+	const minutes = getTimeText(date.getMinutes())
+	const seconds = getTimeText(date.getSeconds())
+	const milliseconds = getTimeText(date.getMilliseconds())
+	return `${year}${month}${day}:${hours}:${minutes}:${seconds}:${milliseconds}`
+}
+
+module.exports = getTime
