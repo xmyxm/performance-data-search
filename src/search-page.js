c019a1d981ba2f786815127266356a70d5bc73ac
chenxuanfeng <chenxuanfeng@meituan.com>
2025-02-13 11:27:30 +0800

    feat: up

--- src/search-page.js
+++ src/search-page.js
@@ -8,9 +8,9 @@ const cookie = 'moa_deviceId=4AC4C0BCA7AD522D89965E6BEC6ADBA4; s_u_745896=NLgW0W
 
 const pageConfigList = [
     {
-        name: '商户详情页',
-        path: '/pages/poi/poi',
-        pageId: '6319467'
+        name: '首页',
+        path: '/pages/home/home',
+        pageId: '663598'
     },
     {
         name: '优惠搜',
@@ -18,9 +18,54 @@ const pageConfigList = [
         pageId: '524321'
     },
     {
-        name: '首页',
-        path: '/pages/home/home',
-        pageId: '663598'
+        name: '商户详情页(美食、休娱)',
+        path: '/pages/poi/poi',
+        pageId: '6319467'
+    },
+    {
+        name: '商户详情页(其它类目)',
+        path: 'packages/detail/pages/detail/detail',
+        pageId: '11492'
+    },
+    {
+        name: '美食频道页',
+        path: 'packages/search/pages/foodchannel/foodchannel',
+        pageId: '3720556'
+    },
+    {
+        name: '美食搜索结果页',
+        path: 'packages/search/pages/searchresultlist/searchresultlist',
+        pageId: '6550184'
+    },
+    {
+        name: '其它类目搜索结果页',
+        path: 'packages/search/pages/list/list',
+        pageId: '11510'
+    },
+    {
+        name: '搜索热词页',
+        path: 'pages/websearch/websearch',
+        pageId: '5275619'
+    },
+    {
+        name: '分店列表（餐、综）',
+        path: 'packages/branch/pages/list/list',
+        pageId: '11504'
+    },
+    {
+        name: '美食分店列表（餐：神会员）',
+        path: 'packages/branch/pages/mslist/mslist',
+        pageId: '13343855'
+    },
+    {
+        name: '商户相册页',
+        path: 'packages/detail/pages/shopalbum/shopalbum',
+        pageId: '9935418'
+    },
+    {
+        name: '好友来过列表页',
+        path: 'packages/friends/pages/shopcome/index',
+        pageId: '11519'
     },
     {
         name: '个人中心',
@@ -43,61 +88,41 @@ const pageConfigList = [
         pageId: '11406'
     },
     {
-        name: '商户详情页2',
-        path: 'packages/detail/pages/detail/detail',
-        pageId: '11492'
+        name: '我的订单列表',
+        path: 'packages/order/pages/myorder/myorder',
+        pageId: '11457'
     },
     {
-        name: '美食频道页',
-        path: 'packages/search/pages/foodchannel/foodchannel',
-        pageId: '3720556'
+        name: '我的收藏列表',
+        path: 'packages/my/pages/usercollection/usercollection',
+        pageId: '5448020'
     },
     {
-        name: '搜索热词页',
-        path: 'pages/websearch/websearch',
-        pageId: '5275619'
+        name: '个人主页',
+        path: 'packages/user/pages/personal/personal',
+        pageId: '11473'
     },
     {
-        name: '美食搜索结果页',
-        path: 'packages/search/pages/searchresultlist/searchresultlist',
-        pageId: '6550184'
-    },
-    {
-        name: '其它类目搜索结果页',
-        path: 'packages/search/pages/list/list',
-        pageId: '11510'
-    },
-    {
-        name: '分店列表（餐、综）',
-        path: 'packages/branch/pages/list/list',
-        pageId: '11504'
-    },
-    {
-        name: '美食分店列表（餐：神会员）',
-        path: 'packages/branch/pages/mslist/mslist',
-        pageId: '13343855'
-    },
-    {
-        name: '商户相册页',
-        path: 'packages/detail/pages/shopalbum/shopalbum',
-        pageId: '9935418'
+        name: '我的点评名片页',
+        path: 'packages/user/pages/card/card',
+        pageId: '11476'
     },
 ]
 
 // 1737362760000
 // 1737364560000
-const startTime = (new Date('2025-01-19')).getTime()
-const endTime = (new Date('2025-01-20')).getTime()
+const startTime = (new Date('2025-02-10')).getTime()
+const endTime = (new Date('2025-02-12')).getTime()
 
-Promise.all(pageConfigList.map(({ path, pageId }) => {
+Promise.all(pageConfigList.map(({ name, path, pageId }) => {
     const projectId = 2621
     const metricList = ['AVG', 'TP50', 'TP90']
-    const data = { path }
+    const data = { name, path }
     const promiseList = []
-    metricList.forEach((metric) => {
-        promiseList.push(fetchPerformanceAPIData(1739356860000, 1739358660000, projectId, pageId, metric).then(val => { data[metric] = val }))
-        promiseList.push(fetchDurationDistributionAPIData(1739356860000, 1739358660000, projectId, pageId).then(val => { data.second = val }))
-        promiseList.push(fetchDimensionDistributionAPIData(1739356860000, 1739358660000, projectId, pageId).then(val => { data.ios = val.ios; data.android = val.android }))
+    metricList.forEach((metric, index) => {
+        promiseList.push(fetchPerformanceAPIData(startTime, endTime, projectId, pageId, metric).then(val => { data[(index + 1) + '.' + metric] = val }))
+        promiseList.push(fetchDurationDistributionAPIData(startTime, endTime, projectId, pageId).then(val => { data['4.second'] = val }))
+        promiseList.push(fetchDimensionDistributionAPIData(startTime, endTime, projectId, pageId).then(val => { data['5.ios'] = val.ios; data['6.android'] = val.android }))
     })
     return Promise.all(promiseList).then(() => data)
 })).then((list) => {
@@ -108,6 +133,13 @@ function calculateAverage(arr) {
     if (arr.length === 0) return 0; // 防止除以零的情况
     const sum = arr.reduce((accum, current) => accum + current, 0);
     return sum / arr.length;
+}
+
+function roundToTwoDecimals(num) {
+    if (num) {
+        return Math.round(num * 100) / 100; // 四舍五入并保留两位小数
+    }
+    return 0
 }
 
 // AVG、TP50、TP90 指标查询
@@ -135,9 +167,9 @@ async function fetchPerformanceAPIData(start,end, projectId, pageId, metric) {
             const { result } = response.data
             if (result.columns && result.rows) {
                 const name = result.columns[1]
-                val = calculateAverage(result.rows.map((item) => {
+                val = roundToTwoDecimals(calculateAverage(result.rows.map((item) => {
                     return item[name]
-                }))
+                })))
             }
         }
         return val
@@ -178,7 +210,9 @@ async function fetchDurationDistributionAPIData(start,end, projectId, pageId, me
                 const mini = resultList.filter((item) => durationNameList.indexOf(item.duration) > -1).reduce((accumulator, current) => {
                     return accumulator + current.onReady; // 累加 value 字段
                 }, 0);
-                val = mini / count
+                if (mini && count) {
+                    val = Math.floor(100 * mini / count) / 100
+                }
             }
         }
         return val
